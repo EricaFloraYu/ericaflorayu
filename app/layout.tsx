2c9ff8013499cb64c60d8dc5e5ef4a909ece4f61
EricaFloraYu <erica@ericaflorayu.com>
2026-05-31 19:23:37 -0400

    add noindex meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: site.title,
   description: site.description,
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
